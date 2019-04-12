74e87e0f66982523374c82df0f8d0061b9cd2be6
Andrii Heha <gega82@gmail.com>
2019-04-13 01:44:08 +0300

    Fixes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,11 +5,13 @@
 public class Calculator {
 
     public static void main(String[] args) {
+
     }
 
     public int add(int a, int b) {
 
         return a + b;
+
     }
 
     public int multiply(int a, int b) {

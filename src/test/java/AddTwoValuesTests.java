3468f238fa5956e559d53d1bac693137b524965e
Andrii Heha <aheha@playtika.com>
2019-05-15 00:06:29 +0300

    Testing the Jenkins

--- src/test/java/AddTwoValuesTests.java
+++ src/test/java/AddTwoValuesTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 /**
- * Created by Agega on 1/12/16.
+ * Created by Agega on 1/12/16.I'm testing the Jenkins
  */
 
 public class AddTwoValuesTests {
